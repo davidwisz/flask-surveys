cff919a8e3c96697ee323e100fe6c6080559fe27
DAVID WISZ <david@wisz.com>
2020-01-23 14:41:36 -0800

    a bit more functionality added, dribs and drabs, a few steps toward the goal

--- app.py
+++ app.py
@@ -15,3 +15,11 @@ def display_survey():
     instructions = satisfaction_survey.instructions
     return render_template("survey.html", title=title, instructions=instructions)
 
+@app.route("/questions/<int:question_num>")
+def display_question(question_num):
+    question = satisfaction_survey.questions[question_num]
+    title = f"Question {question_num + 1}"
+    return render_template("survey-question.html", title=title, question=question)
+    # question = satisfaction_survey.title
+    # instructions = satisfaction_survey.instructions
+    # return render_template("survey.html", title=title, instructions=instructions)
