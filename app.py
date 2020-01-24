f8a2fd0642d4a8e93e24ef78503b10f79e54d570
DAVID WISZ <david@wisz.com>
2020-01-23 17:11:51 -0800

    fixed responses in session

--- app.py
+++ app.py
@@ -24,19 +24,18 @@ def initiate_session():
 
 @app.route("/questions/<int:question_num>")
 def display_question(question_num):
-    #import pdb; pdb.set_trace()
-    if question_num != len(session["responses"]):
-        responses = session["responses"]
+    the_answers = session["responses"]
+    decoded_responses = the_answers
+    if question_num != len(the_answers):
         flash("You are missing an answer!")
-        #breakpoint()
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(the_answers)}")
 
     question_instance = satisfaction_survey.questions[question_num]
     question_text = question_instance.question
     choices = question_instance.choices
     title = f"Question {question_num + 1}"
 
-    return render_template("survey-question.html", title=title, question=question_text, choices=choices, question_num=question_num)
+    return render_template("survey-question.html", title=title, question=question_text, choices=choices, question_num=question_num, decoded_responses=decoded_responses)
 
 @app.route("/answer", methods=["POST"])
 def process_answer():
@@ -45,9 +44,9 @@ def process_answer():
     question_num = int(request.form['question_num']) + 1
 
     if question_num < len(question_instance):
-        responses = session["responses"]
-        responses.append(request.form['question'])
-        session["responses"] = responses
+        the_answers = session["responses"]
+        the_answers.append(request.form["question"])
+        session["responses"] = the_answers
         destination = f"/questions/{question_num}"
     else:
         destination = "/thank-you"
